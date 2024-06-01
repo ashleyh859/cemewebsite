79f6e945fe1bdf81d1a5ab839447d1d6bcae3363
Ashley Huang <138391564+ashleyh859@users.noreply.github.com>
2024-06-01 00:13:12 +0000

    fixed accordion

--- scripts/accordion.js
+++ scripts/accordion.js
@@ -1,104 +1,11 @@
 $(document).ready(function () {
-  $('#header1').click(function () {
-    $('.accordion-content').not('#content1').addClass('accessible-hidden');
-    $('.icon').not('#icon1').removeClass('rotate-icon');
-    $('#content1').toggleClass('accessible-hidden')
+  $('#header1').on('click', function() {
     if ($('#content1').hasClass('accessible-hidden')) {
-      $('#icon1').removeClass('rotate-icon');
+      $('#content1').removeClass('accessible-hidden');
+      $('#icon1').text('−');
     } else {
-      $('#icon1').addClass('rotate-icon');
-    }
-  });
-
-});
-
-$(document).ready(function () {
-  $('#header2').click(function () {
-    $('.accordion-content').not('#content2').addClass('accessible-hidden');
-    $('.icon').not('#icon2').removeClass('rotate-icon');
-    $('#content2').toggleClass('accessible-hidden')
-    if ($('#content2').hasClass('accessible-hidden')) {
-      $('#icon2').removeClass('rotate-icon');
-    } else {
-      $('#icon2').addClass('rotate-icon');
-    }
-  });
-});
-
-$(document).ready(function () {
-  $('#header3').click(function () {
-    $('.accordion-content').not('#content3').addClass('accessible-hidden');
-    $('.icon').not('#icon3').removeClass('rotate-icon');
-    $('#content3').toggleClass('accessible-hidden')
-    if ($('#content3').hasClass('accessible-hidden')) {
-      $('#icon3').removeClass('rotate-icon');
-    } else {
-      $('#icon3').addClass('rotate-icon');
-    }
-  });
-});
-
-$(document).ready(function () {
-  $('#header4').click(function () {
-    $('.accordion-content').not('#content4').addClass('accessible-hidden');
-    $('.icon').not('#icon4').removeClass('rotate-icon');
-    $('#content4').toggleClass('accessible-hidden')
-    if ($('#content4').hasClass('accessible-hidden')) {
-      $('#icon4').removeClass('rotate-icon');
-    } else {
-      $('#icon4').addClass('rotate-icon');
-    }
-  });
-});
-
-$(document).ready(function () {
-  $('#header5').click(function () {
-    $('.accordion-content').not('#content5').addClass('accessible-hidden');
-    $('.icon').not('#icon5').removeClass('rotate-icon');
-    $('#content5').toggleClass('accessible-hidden')
-    if ($('#content5').hasClass('accessible-hidden')) {
-      $('#icon5').removeClass('rotate-icon');
-    } else {
-      $('#icon5').addClass('rotate-icon');
-    }
-  });
-});
-
-$(document).ready(function () {
-  $('#header6').click(function () {
-    $('.accordion-content').not('#content6').addClass('accessible-hidden');
-    $('.icon').not('#icon6').removeClass('rotate-icon');
-    $('#content6').toggleClass('accessible-hidden')
-    if ($('#content6').hasClass('accessible-hidden')) {
-      $('#icon6').removeClass('rotate-icon');
-    } else {
-      $('#icon6').addClass('rotate-icon');
-    }
-  });
-});
-
-$(document).ready(function () {
-  $('#header7').click(function () {
-    $('.accordion-content').not('#content7').addClass('accessible-hidden');
-    $('.icon').not('#icon7').removeClass('rotate-icon');
-    $('#content7').toggleClass('accessible-hidden')
-    if ($('#content7').hasClass('accessible-hidden')) {
-      $('#icon7').removeClass('rotate-icon');
-    } else {
-      $('#icon7').addClass('rotate-icon');
-    }
-  });
-});
-
-$(document).ready(function () {
-  $('#header8').click(function () {
-    $('.accordion-content').not('#content8').addClass('accessible-hidden');
-    $('.icon').not('#icon8').removeClass('rotate-icon');
-    $('#content8').toggleClass('accessible-hidden')
-    if ($('#content8').hasClass('accessible-hidden')) {
-      $('#icon8').removeClass('rotate-icon');
-    } else {
-      $('#icon8').addClass('rotate-icon');
+      $('#content1').addClass('accessible-hidden');
+      $('#icon1').text('+');
     }
   });
 });
