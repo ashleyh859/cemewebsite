dbae8d92aed42ac69b2a57d188ea1c93f57eb22a
Ashley Huang <138391564+ashleyh859@users.noreply.github.com>
2024-06-02 19:24:17 +0000

    fixed minor errors: carousel paddng, text and image changes

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -3,44 +3,65 @@ $("#button1").click(function() {
     $("#button2").removeClass("active")
     $("#button3").removeClass("active")
     $("#button4").removeClass("active")
+    $("#button5").removeClass("active")
 
     $("#image1").removeClass("accessible-hidden")
     $("#image2").addClass("accessible-hidden")
     $("#image3").addClass("accessible-hidden")
     $("#image4").addClass("accessible-hidden")
+    $("#image5").addClass("accessible-hidden")
   });
   $("#button2").click(function() {
     $("#button1").removeClass("active")
     $("#button2").addClass("active")
     $("#button3").removeClass("active")
     $("#button4").removeClass("active")
+    $("#button5").removeClass("active")
 
     $("#image1").addClass("accessible-hidden")
     $("#image2").removeClass("accessible-hidden")
     $("#image3").addClass("accessible-hidden")
     $("#image4").addClass("accessible-hidden")
+    $("#image5").addClass("accessible-hidden")
   });
   $("#button3").click(function() {
     $("#button1").removeClass("active")
     $("#button2").removeClass("active")
     $("#button3").addClass("active")
     $("#button4").removeClass("active")
+    $("#button5").removeClass("active")
 
     $("#image1").addClass("accessible-hidden")
     $("#image2").addClass("accessible-hidden")
     $("#image3").removeClass("accessible-hidden")
     $("#image4").addClass("accessible-hidden")
+    $("#image5").addClass("accessible-hidden")
   });
   $("#button4").click(function() {
     $("#button1").removeClass("active")
     $("#button2").removeClass("active")
     $("#button3").removeClass("active")
     $("#button4").addClass("active")
+    $("#button5").removeClass("active")
 
     $("#image1").addClass("accessible-hidden")
     $("#image2").addClass("accessible-hidden")
     $("#image3").addClass("accessible-hidden")
     $("#image4").removeClass("accessible-hidden")
+    $("#image5").addClass("accessible-hidden")
+  });
+  $("#button5").click(function() {
+    $("#button1").removeClass("active")
+    $("#button2").removeClass("active")
+    $("#button3").removeClass("active")
+    $("#button4").removeClass("active")
+    $("#button5").addClass("active")
+
+    $("#image1").addClass("accessible-hidden")
+    $("#image2").addClass("accessible-hidden")
+    $("#image3").addClass("accessible-hidden")
+    $("#image4").addClass("accessible-hidden")
+    $("#image5").removeClass("accessible-hidden")
   });
 
 $("#prev-button").click(function() {
@@ -64,7 +85,7 @@ $("#prev-button").click(function() {
         $("#button1").addClass("active");
         $("#image1").removeClass("accessible-hidden");
         $("#image2").addClass("accessible-hidden")
-    }
+    } 
   });
   $("#next-button").click(function() {
     if ($("#button1").hasClass("active")) {
