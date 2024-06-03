66b037a3601cf41c5e19161bfbe564cd4db9df3c
Ashley Huang <138391564+ashleyh859@users.noreply.github.com>
2024-06-03 18:01:05 +0000

    fixed carousel

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -4,12 +4,14 @@ $("#button1").click(function() {
     $("#button3").removeClass("active")
     $("#button4").removeClass("active")
     $("#button5").removeClass("active")
+    $("#button6").removeClass("active")
 
     $("#image1").removeClass("accessible-hidden")
     $("#image2").addClass("accessible-hidden")
     $("#image3").addClass("accessible-hidden")
     $("#image4").addClass("accessible-hidden")
     $("#image5").addClass("accessible-hidden")
+    $("#image6").addClass("accessible-hidden")
   });
   $("#button2").click(function() {
     $("#button1").removeClass("active")
@@ -17,12 +19,14 @@ $("#button1").click(function() {
     $("#button3").removeClass("active")
     $("#button4").removeClass("active")
     $("#button5").removeClass("active")
+    $("#button6").removeClass("active")
 
     $("#image1").addClass("accessible-hidden")
     $("#image2").removeClass("accessible-hidden")
     $("#image3").addClass("accessible-hidden")
     $("#image4").addClass("accessible-hidden")
     $("#image5").addClass("accessible-hidden")
+    $("#image6").addClass("accessible-hidden")
   });
   $("#button3").click(function() {
     $("#button1").removeClass("active")
@@ -30,12 +34,14 @@ $("#button1").click(function() {
     $("#button3").addClass("active")
     $("#button4").removeClass("active")
     $("#button5").removeClass("active")
+    $("#button6").removeClass("active")
 
     $("#image1").addClass("accessible-hidden")
     $("#image2").addClass("accessible-hidden")
     $("#image3").removeClass("accessible-hidden")
     $("#image4").addClass("accessible-hidden")
     $("#image5").addClass("accessible-hidden")
+    $("#image6").addClass("accessible-hidden")
   });
   $("#button4").click(function() {
     $("#button1").removeClass("active")
@@ -43,12 +49,14 @@ $("#button1").click(function() {
     $("#button3").removeClass("active")
     $("#button4").addClass("active")
     $("#button5").removeClass("active")
+    $("#button6").removeClass("active")
 
     $("#image1").addClass("accessible-hidden")
     $("#image2").addClass("accessible-hidden")
     $("#image3").addClass("accessible-hidden")
     $("#image4").removeClass("accessible-hidden")
     $("#image5").addClass("accessible-hidden")
+    $("#image6").addClass("accessible-hidden")
   });
   $("#button5").click(function() {
     $("#button1").removeClass("active")
@@ -56,57 +64,96 @@ $("#button1").click(function() {
     $("#button3").removeClass("active")
     $("#button4").removeClass("active")
     $("#button5").addClass("active")
+    $("#button6").removeClass("active")
 
     $("#image1").addClass("accessible-hidden")
     $("#image2").addClass("accessible-hidden")
     $("#image3").addClass("accessible-hidden")
     $("#image4").addClass("accessible-hidden")
     $("#image5").removeClass("accessible-hidden")
+    $("#image6").addClass("accessible-hidden")
+  });
+  $("#button6").click(function() {
+    $("#button1").removeClass("active")
+    $("#button2").removeClass("active")
+    $("#button3").removeClass("active")
+    $("#button4").removeClass("active")
+    $("#button5").removeClass("active")
+    $("#button6").addClass("active")
+
+    $("#image1").addClass("accessible-hidden")
+    $("#image2").addClass("accessible-hidden")
+    $("#image3").addClass("accessible-hidden")
+    $("#image4").addClass("accessible-hidden")
+    $("#image5").addClass("accessible-hidden")
+    $("#image6").removeClass("accessible-hidden")
   });
 
-$("#prev-button").click(function() {
+  $("#prev-button").click(function() {
     if ($("#button1").hasClass("active")) {
       $("#button1").removeClass("active");
+      $("#button6").addClass("active");
+      $("#image6").removeClass("accessible-hidden");
+      $("#image1").addClass("accessible-hidden");
+    } else if ($("#button6").hasClass("active")) {
+      $("#button6").removeClass("active");
+      $("#button5").addClass("active");
+      $("#image5").removeClass("accessible-hidden");
+      $("#image6").addClass("accessible-hidden");
+    } else if ($("#button5").hasClass("active")) {
+      $("#button5").removeClass("active");
       $("#button4").addClass("active");
       $("#image4").removeClass("accessible-hidden");
-      $("#image1").addClass("accessible-hidden")
+      $("#image5").addClass("accessible-hidden");
     } else if ($("#button4").hasClass("active")) {
-        $("#button4").removeClass("active");
-        $("#button3").addClass("active");
-        $("#image3").removeClass("accessible-hidden");
-        $("#image4").addClass("accessible-hidden")
+      $("#button4").removeClass("active");
+      $("#button3").addClass("active");
+      $("#image3").removeClass("accessible-hidden");
+      $("#image4").addClass("accessible-hidden");
     } else if ($("#button3").hasClass("active")) {
-        $("#button3").removeClass("active");
-        $("#button2").addClass("active");
-        $("#image2").removeClass("accessible-hidden");
-        $("#image3").addClass("accessible-hidden")
+      $("#button3").removeClass("active");
+      $("#button2").addClass("active");
+      $("#image2").removeClass("accessible-hidden");
+      $("#image3").addClass("accessible-hidden");
     } else if ($("#button2").hasClass("active")) {
-        $("#button2").removeClass("active");
-        $("#button1").addClass("active");
-        $("#image1").removeClass("accessible-hidden");
-        $("#image2").addClass("accessible-hidden")
-    } 
+      $("#button2").removeClass("active");
+      $("#button1").addClass("active");
+      $("#image1").removeClass("accessible-hidden");
+      $("#image2").addClass("accessible-hidden");
+    }
   });
+
   $("#next-button").click(function() {
     if ($("#button1").hasClass("active")) {
       $("#button1").removeClass("active");
       $("#button2").addClass("active");
       $("#image2").removeClass("accessible-hidden");
-      $("#image1").addClass("accessible-hidden")
+      $("#image1").addClass("accessible-hidden");
     } else if ($("#button2").hasClass("active")) {
-        $("#button2").removeClass("active");
-        $("#button3").addClass("active");
-        $("#image3").removeClass("accessible-hidden");
-        $("#image2").addClass("accessible-hidden")
+      $("#button2").removeClass("active");
+      $("#button3").addClass("active");
+      $("#image3").removeClass("accessible-hidden");
+      $("#image2").addClass("accessible-hidden");
     } else if ($("#button3").hasClass("active")) {
-        $("#button3").removeClass("active");
-        $("#button4").addClass("active");
-        $("#image4").removeClass("accessible-hidden");
-        $("#image3").addClass("accessible-hidden")
+      $("#button3").removeClass("active");
+      $("#button4").addClass("active");
+      $("#image4").removeClass("accessible-hidden");
+      $("#image3").addClass("accessible-hidden");
     } else if ($("#button4").hasClass("active")) {
-        $("#button4").removeClass("active");
-        $("#button1").addClass("active");
-        $("#image1").removeClass("accessible-hidden");
-        $("#image4").addClass("accessible-hidden")
+      $("#button4").removeClass("active");
+      $("#button5").addClass("active");
+      $("#image5").removeClass("accessible-hidden");
+      $("#image4").addClass("accessible-hidden");
+    } else if ($("#button5").hasClass("active")) {
+      $("#button5").removeClass("active");
+      $("#button6").addClass("active");
+      $("#image6").removeClass("accessible-hidden");
+      $("#image5").addClass("accessible-hidden");
+    } else if ($("#button6").hasClass("active")) {
+      $("#button6").removeClass("active");
+      $("#button1").addClass("active");
+      $("#image1").removeClass("accessible-hidden");
+      $("#image6").addClass("accessible-hidden");
     }
   });
+
